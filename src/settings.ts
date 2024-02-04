1804346788b236d61a5118126748c94d0409692c
max <max@pop-os.localdomain>
2024-02-04 15:11:10 +0100

    added simple settings GUI

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,5 @@
-
+import Checklists from "./main";
+import { App, PluginSettingTab, Setting } from "obsidian";
 
 export interface ChecklistSettings {
     source_folder: string;
@@ -8,3 +9,33 @@ export interface ChecklistSettings {
 export const DEFAULT_SETTINGS: Partial<ChecklistSettings> = {
     source_folder: "/"
 };
+
+export class ChecklistsSettingsTab extends PluginSettingTab {
+    plugin: Checklists;
+
+    constructor(app: App, plugin: Checklists) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        let { containerEl } = this;
+
+        console.log(containerEl);
+
+        containerEl.empty();
+
+        new Setting(containerEl)
+            .setName("Source Folder")
+            .setDesc("Add a Source Folder")
+            .addText((text) =>
+                text
+                    .setPlaceholder("something")
+                    .setValue(this.plugin.settings.source_folder)
+                    .onChange(async (value) => {
+                        this.plugin.settings.source_folder = value;
+                        await this.plugin.save_settings();
+                    })
+            );
+    }
+}
